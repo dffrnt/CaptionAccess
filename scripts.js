46d45779627d0ff41d3ad254c48a3649889430b9
vjk2005 <vijay@dffrnt.com>
2014-03-25 02:01:45 +0530

    Complete site
    Final layout and data entry update.

--- scripts.js
+++ scripts.js
@@ -3,7 +3,10 @@ var
 	$window 	= $( window ),
 	$header  	= $( '#header' ),
 	$navbar  	= $( '#nav-bar' ),
+	$logo 		= $( '#logo' ),
+	$navlinks 	= $( '.level1item' ),
 	$wrapper 	= $( '#wrapper' ),
+	$button 	= $( '#call-to-action' ),
 	$features 	= $( '#call-to-action-areas .component4' ),
 	scrollThreshold,
 	scrollRatio
@@ -13,18 +16,33 @@ function reposition() {
 	scrollThreshold = $navbar.offset().top + $navbar.height()/2 // calculate scrollThreshold ONLY after the header has been repositioned
 }
 
+$button.on( 'click', function() { window.location = 'contact.html' })
+$logo.on( 'click', function() { window.location = 'index.html' })
+
 $window
 	.on( 'load resize', reposition )
-	.on( 'scroll', function() {
+	.on( 'scroll', function() { // nav-bar
 		scrollRatio = $window.scrollTop()/$header.parent().outerHeight(true)
 		$window.scrollTop() > scrollThreshold? $navbar.addClass( 'fixed' ) : $navbar.removeClass( 'fixed' ) // fixed nav bar
 		$( '#header' ).parent().css( 'background-color', 'rgba(44, 44, 44, ' + scrollRatio + ')' )
 	})
 
+// nav menu
+var $activeMenu = ''
+$navlinks.on( 'mouseenter', function() {
+	$activeMenu = $( $(this).children()[0] )
+	$activeMenu.addClass( 'active' ).parent().find( 'ul.submenu' ).slideDown(300)
+}).on( 'mouseleave', function() {
+	$activeMenu.removeClass( 'active' ).parent().find( 'ul.submenu' ).slideUp(50)
+	$activeMenu = ''
+})
+
 // testimonial slideshow
+var direction = '-'
 setInterval( function() {
-	var totalWidth = -($wrapper.children().length - 1) * $wrapper.children().outerWidth(true),
-		direction = !$wrapper.css( 'left' )? '-': (parseInt($wrapper.css( 'left' ),10) <= totalWidth? '+':'-')
+	var totalWidth = ($wrapper.children().length - 1) * $wrapper.children().outerWidth(true),
+		currentPos = parseInt( $wrapper.css('left'), 10 )
+	!currentPos? direction = '-' : currentPos <= -totalWidth? direction = '+' : ''
 	$wrapper.animate({ 'left': direction + '=960px' }, 800)
 }, 5000)
 
