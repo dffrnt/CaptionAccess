ef8896af62a38b329cbe31521b22a93aa0445019
vjk2005 <vijay@dffrnt.com>
2014-05-07 23:21:28 +0530

    Responsive design phase 2
    Completed responsive designs for
    - navbar
    - dropdown menus inside navbar
    - homepage call-to-action type-in widget
    - content grid
    - testimonial slideshow
    - footer
    - social media icons
    
    Added arrows for testimonial slideshow
    New logo for smartphone screens (responsive logo design)
    This phase has the complete responsive design implementation for the
    homepage (index.html) and about page (about.html).

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
-function isXS() {
-    /* mobile */
-    return $('.device-xs').is(':visible')
-}
+var isXS            = 	function() {
+							/* mobile */
+							return $('.device-xs').is(':visible')
+						}
 
-function isSM() {
-    /* tablet */
-    return $('.device-sm').is(':visible')
-}
+var isSM            = 	function() {
+							/* tablet */
+							return $('.device-sm').is(':visible')
+						}
 
-function isMD() {
-    /* desktop */
-    return $('.device-md').is(':visible')
-}
+var isMD            = 	function() {
+							/* desktop */
+							return $('.device-md').is(':visible')
+						}
 
-$(window).on('resize load', function() {
-    if($(window).width() <= 1030) {
-        /* add mobile-specific customizations */
-        /* 1. fix the navbar to the top */
-        $('.navbar').addClass('navbar-fixed-top')
-    } else {
-        /* remove all mobile-specific customizations */
-        $('.navbar').removeClass('navbar-fixed-top')
-    }
-})
+var header			= 	$('#header'),
+	btnCTA			= 	$('#header.inner button'),
+	headerMsg 		=   $('#header p')
 
+var verticalCenter  = 	function() {
+							btnCTA.css({'margin-top': (header.height() - btnCTA.outerHeight())/2})
+							headerMsg.css({'margin-top': (header.height() - headerMsg.outerHeight())/2})
+						}
 
-$(document).ready(function(){
+var headerAdjust    = 	function(mode) {
+							$('#content-grid').css({'margin-top': mode? 60 : header.height() + 120})
+						}
+
+var adjust 			= 	function() {
+							headerAdjust()
+							verticalCenter()
+						}
+
+$(function() {
+	adjust()
 	$('#testimonials').slick({
 		autoplay: true,
+		draggable: false,
 		autoplaySpeed: 10000
 	})
 })
 
-$( '#call-to-action-areas .component4' ).typed({
+$(window).on('resize load', function() {
+	adjust()
+	if($(window).width() <= 1100) {
+		/* add mobile-specific customizations */
+		/* 1. fix the navbar to the top */
+		$('.navbar').addClass('navbar-fixed-top')
+	} else {
+		/* remove all mobile-specific customizations */
+		$('.navbar').removeClass('navbar-fixed-top')
+		headerAdjust('reset')
+	}
+})
+
+$( '#call-to-action-areas #area2' ).typed({
 	strings: ["Live captioning", "Media captioning", "Transcriptions"],
 	typeSpeed: 20,
 	backDelay: 4000,
