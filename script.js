d689a84f32ee99c784e9a46cc6fb1e5b74695afb
vjk2005 <vijay@dffrnt.com>
2014-05-23 00:06:01 +0530

    Navbar fix for Nexus 4 and similar smartphones
    Also fixes the transparency and readability issue on the homepage.

--- script.js
+++ script.js
@@ -16,23 +16,24 @@ var isMD            = 	function() {
 var header			= 	$('#header'),
 	btnCTA			= 	$('#header.inner button'),
 	headerMsg 		=   $('#header p')
+	headerHeight 	= 	header.height()
 
 var verticalCenter  = 	function() {
-							btnCTA.css({'margin-top': (header.height() - btnCTA.outerHeight())/2})
-							headerMsg.css({'margin-top': (header.height() - headerMsg.outerHeight())/2})
+							btnCTA.css({'margin-top': (headerHeight - btnCTA.outerHeight())/2})
+							headerMsg.css({'margin-top': (headerHeight - headerMsg.outerHeight())/2})
 						}
 
 var headerAdjust    = 	function(mode) {
-							$('#content-grid').css({'margin-top': mode? 60 : header.height() + 120})
+							$('#content-grid').css({'margin-top': mode? 60 : headerHeight + 120})
 						}
-
-var adjust 			= 	function() {
-							headerAdjust()
-							verticalCenter()
+var opacityAdjust 	=	function() {
+							$('.navbar.default .navbar-toggle').on('click', function() {
+								!$('.navbar.default div.in').length? $('.navbar-fixed-top').addClass('opaque') : $('.navbar-fixed-top').removeClass('opaque')
+							})
 						}
 
 $(function() {
-	adjust()
+	opacityAdjust()
 	$('#testimonials').slick({
 		autoplay: true,
 		draggable: false,
@@ -41,21 +42,26 @@ $(function() {
 })
 
 $(window).on('resize load', function() {
-	adjust()
+	headerHeight = header.height()
 	if($(window).width() <= 1100) {
-		/* add mobile-specific customizations */
-		/* 1. fix the navbar to the top */
-		$('.navbar').addClass('navbar-fixed-top')
+		headerAdjust()
+		if($(window).width() > 768) verticalCenter()
+		/* fix the navbar to the top */
+		$('.navbar.default').addClass('navbar-fixed-top')
 	} else {
 		/* remove all mobile-specific customizations */
 		$('.navbar').removeClass('navbar-fixed-top')
+		verticalCenter()
 		headerAdjust('reset')
 	}
 })
 
-$( '#call-to-action-areas #area2' ).typed({
-	strings: ["Live captioning", "Media captioning", "Transcriptions"],
-	typeSpeed: 20,
-	backDelay: 4000,
-	loop: true
-})
+var featureShow = $( '#call-to-action-areas #area2' )
+if(featureShow.length) {
+	featureShow.typed({
+		strings: ["Live captioning", "Media captioning", "Transcriptions"],
+		typeSpeed: 20,
+		backDelay: 4000,
+		loop: true
+	})
+}
